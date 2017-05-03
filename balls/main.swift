405f85a20d4f1ceb467f9fd01cd902b1bab028a0
Nick <patsichat@gmail.com>
2017-05-03 14:29:08 +0700

    Initial Commit

---
+++ balls/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  balls
+//
+//  Created by Patsicha Tongteeka on 5/3/17.
+//  Copyright © 2017 Nick. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
