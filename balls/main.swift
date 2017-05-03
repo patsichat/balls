887109b1e8a393451464755e9ede440f1c9c1a27
Nick <patsichat@gmail.com>
2017-05-03 15:08:08 +0700

    Added code for calculate position of balls

--- balls/main.swift
+++ balls/main.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+var balls = ["1","2","3","4","5","6","7","8"]
+
+var input = readLine()
+let numberOfOperations = Int(input!.components(separatedBy: " ")[0])!
+let numberOfSets = Int(input!.components(separatedBy: " ")[1])!
+
+var commands = [[Int]]()
+for i in 1...numberOfOperations {
+    var command = readLine()
+    let firstBall = Int(command!.components(separatedBy: " ")[0])! - 1
+    let secondBall = Int(command!.components(separatedBy: " ")[1])! - 1
+    commands += [[firstBall,secondBall]]
+}
+
+for setIndex in 1...numberOfSets{
+    for command in commands {
+        let temp = balls[command[0]]
+        balls[command[0]] = balls[command[1]]
+        balls[command[1]] = temp
+    }
+}
+
+print(balls.joined(separator: " "))
